45fc062b36e003c2eb0e934d4c189afcd21d4f1a
Anton Lopan <Anton Lopan>
2016-07-03 18:07:53 +0300

    Pre physical

--- public/js/main2.js
+++ public/js/main2.js
@@ -1,17 +1,10 @@
+'use strict';
+
+Physijs.scripts.worker = '/js/physijs_worker.js';
+
+
+var camera, scene, renderer, startTime, object;
 var container, stats;
-var camera, scene, renderer;
-var group;
-var targetRotation = 0;
-var targetRotationOnMouseDown = 0;
-var mouseX = 0;
-var mouseXOnMouseDown = 0;
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-
-
-init();
-animate();
-
 function init() {
 	container = document.createElement( 'div' );
 	document.body.appendChild( container );
@@ -21,126 +14,86 @@ function init() {
 	info.style.width = '100%';
 	info.style.textAlign = 'center';
 	container.appendChild( info );
+
+
+	camera = new THREE.PerspectiveCamera(
+			90, window.innerWidth / window.innerHeight, 0.25, 1000 );
+	camera.position.set( 11.441660950109432,31.978614483870004,6.166577442586645 );
 	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1000 );
-	camera.position.set( 0, 150, 500 );
-	scene.add( camera );
-	var light = new THREE.PointLight( 0xffffff, 0.8 );
-	camera.add( light );
-	group = new THREE.Group();
-	group.position.y = 50;
-	scene.add( group );
+	// Lights
+	scene.add( new THREE.AmbientLight( 0x505050 ) );
+	var spotLight = new THREE.SpotLight( 0xffffff );
+	spotLight.angle = Math.PI / 5;
+	spotLight.penumbra = 0.2;
+	spotLight.position.set( 11.441660950109432,31.978614483870004,6.166577442586645 );
+	spotLight.castShadow = true;
+	spotLight.shadow.camera.near = 3;
+	spotLight.shadow.camera.far = 10;
+	spotLight.shadow.mapSize.width = 1024;
+	spotLight.shadow.mapSize.height = 1024;
+	scene.add( spotLight );
+	var dirLight = new THREE.DirectionalLight( 0x55505a, 1 );
+	dirLight.position.set( 0, 3, 0 );
+	dirLight.castShadow = true;
+	dirLight.shadow.camera.near = 1;
+	dirLight.shadow.camera.far = 10;
+	dirLight.shadow.camera.right = 1;
+	dirLight.shadow.camera.left = - 1;
+	dirLight.shadow.camera.top	= 1;
+	dirLight.shadow.camera.bottom = - 1;
+	dirLight.shadow.mapSize.width = 1024;
+	dirLight.shadow.mapSize.height = 1024;
+	scene.add( dirLight );
 
+	// Geometry
+	var material = new THREE.MeshPhongMaterial( {
+			color: 0x80ee10,
+			shininess: 100,
+			side: THREE.DoubleSide,
+			clipShadows: true
+		} ),
+	geometry = new THREE.CylinderGeometry( 10, 10, 1, 60 );
 
+	object = new THREE.Mesh( geometry, material );
+	object.position.set(0,10,0);
+	object.castShadow = true;
+	scene.add( object );
 
-	// Arc circle
-	var arcShape = new THREE.Shape();
-	arcShape.moveTo( 50, 10 );
-	arcShape.absarc( 10, 10, 40, 0, Math.PI*2, false );
-
-	var loader = new THREE.TextureLoader();
-	var texture = loader.load( "textures/UV_Grid_Sm.jpg" );
-	var texture2 = loader.load( "textures/UV_Grid_Sm.jpg" );
-	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-	texture.repeat.set( 0.008, 0.008 );
-
-	texture2.wrapS = texture2.wrapT = THREE.RepeatWrapping;
-	texture2.repeat.set( 0.028, 0.058 );
-
-
-	var extrudeSettings = {curveSegments: 40, amount: 4, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
-
-	addShape( arcShape,      extrudeSettings, 0xffffff,  0,  70, 0, 0, 0, 0, 1 );
-
-
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setClearColor( 0xf0f0f0 );
+	var ground = new THREE.Mesh(
+			new THREE.PlaneBufferGeometry( 1000, 1000, 1, 1 ),
+			new THREE.MeshPhongMaterial( {
+				color: 0xa0adaf, shininess: 150 } ) );
+	ground.rotation.x = - Math.PI / 2; // rotates X/Y to X/Z
+	ground.receiveShadow = true;
+	scene.add( ground );
+	// Renderer
+	renderer = new THREE.WebGLRenderer();
+	renderer.shadowMap.enabled = true;
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	container.appendChild( renderer.domElement );
-	stats = new Stats();
-	container.appendChild( stats.dom );
-	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-	document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-	document.addEventListener( 'touchmove', onDocumentTouchMove, false );
 	window.addEventListener( 'resize', onWindowResize, false );
-
+	document.body.appendChild( renderer.domElement );
 	
 
-	function addShape( shape, extrudeSettings, color, x, y, z, rx, ry, rz, s ) {
-		var geometry = new THREE.ShapeGeometry( shape );
-		var mesh = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial( { side: THREE.DoubleSide, map: texture } ) );
-		mesh.position.set( x, y, z - 69 );
-		mesh.rotation.set( rx, ry, rz );
-		mesh.scale.set( s, s, s );
-		group.add( mesh );
-
-		var geometry = new THREE.ShapeGeometry( shape );
-		var mesh = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial( { side: THREE.DoubleSide, map: texture2 } ) );
-		mesh.position.set( x, y, z - 77 );
-		mesh.rotation.set( rx, ry, rz );
-		mesh.scale.set( s, s, s );
-		group.add( mesh );
-
-		// extruded shape
-		var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-		var mesh = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial( { color: color } ) );
-		mesh.position.set( x, y, z - 75 );
-		mesh.rotation.set( rx, ry, rz );
-		mesh.scale.set( s, s, s );
-		group.add( mesh );
-	}
+	var controls = new THREE.OrbitControls( camera, renderer.domElement );
+	controls.target.set( 0, 1, 0 );
+	controls.update();
+	stats = new Stats();
+				container.appendChild( stats.dom );
+	// Start
+	startTime = Date.now();
 }
-
 function onWindowResize() {
-	windowHalfX = window.innerWidth / 2;
-	windowHalfY = window.innerHeight / 2;
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 }
-function onDocumentMouseDown( event ) {
-	event.preventDefault();
-	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-	document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-	document.addEventListener( 'mouseout', onDocumentMouseOut, false );
-	mouseXOnMouseDown = event.clientX - windowHalfX;
-	targetRotationOnMouseDown = targetRotation;
-}
-function onDocumentMouseMove( event ) {
-	mouseX = event.clientX - windowHalfX;
-	targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
-}
-function onDocumentMouseUp( event ) {
-	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-	document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-	document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-}
-function onDocumentMouseOut( event ) {
-	document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
-	document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
-	document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
-}
-function onDocumentTouchStart( event ) {
-	if ( event.touches.length == 1 ) {
-		event.preventDefault();
-		mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
-		targetRotationOnMouseDown = targetRotation;
-	}
-}
-function onDocumentTouchMove( event ) {
-	if ( event.touches.length == 1 ) {
-		event.preventDefault();
-		mouseX = event.touches[ 0 ].pageX - windowHalfX;
-		targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
-	}
-}
 function animate() {
-	requestAnimationFrame( animate );
-	render();
+	var currentTime = Date.now(),
+		time = ( currentTime - startTime ) / 1000;
 	stats.update();
-}
-function render() {
-	group.rotation.y += ( targetRotation - group.rotation.y ) * 0.05;
+	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
+init();
+animate();
